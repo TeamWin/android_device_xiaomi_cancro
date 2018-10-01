8dedd74ac0615b8f9959f547dca1a4647dac3387
MkfsSion <myychina28759@gmail.com>
2018-10-01 10:47:13 +0800

    cancro: Use script to mount firmware
    * Init seems doesn't set proper gid and uid for mounted firmware which causes qseecomd failed to open firmware for decrypt
    
    Signed-off-by: MkfsSion <myychina28759@gmail.com>

---
+++ recovery/root/sbin/decrypt.sh
@@ -0,0 +1,15 @@
+#!/sbin/sh
+
+# Setup mount point for firmware
+if [ ! -d "/firmware" ]; then
+	mkdir "/firmware"
+	chown system:system "/firmware"
+	chmod 0771 "/firmware"
+fi
+
+# Mount fimrware for decrypt
+if [ ! -d "/firmware/image" ]; then
+	mount -t vfat -o ro,shortname=lower,uid=1000,gid=1000,dmask=227,fmask=337 /dev/block/bootdevice/by-name/modem /firmware
+fi
+
+/sbin/qseecomd
